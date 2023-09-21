d54cbcf7639eb8a2118db02196d348029a2f0c9e
Jon Telleria <telleria.jon@uni.eus>
2023-09-21 13:23:54 +0200

    1 ariketa eginda eta bigarren ariketa hasten

---
+++ Ariketa2(ariketa matematikoa)/Ariketa2(ariketa matematikoa)/Form1.cs
@@ -0,0 +1,12 @@
+namespace Ariketa2_ariketa_matematikoa_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
